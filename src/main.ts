79a54ff33aab4a633d3c3d05b2f4681e09078f19
jorge-alvarado-revata <serviciosplusapp@gmail.com>
2025-10-18 01:29:39 -0500

    update version mysql

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('API de Division')
+    .setDescription('Esta es una API para manipular divisiones')
+    .setVersion('1.0')
+    .addTag('divisiones')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 void bootstrap();
